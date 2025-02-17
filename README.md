59565e6772832e7b569076033626f5a42155ef0e
K.Hashimoto <hashimoto.hrse5@gmail.com>
2025-02-17 11:41:28 +0900

    lesson1の問3、lesson4の問5を修正しました。

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # lesson_php
+lesson1
+lesson2
+lesson3
+lesson4
+
+2/17
+lesson1.php　3.　
+リンゴとみかんの単価の初期値を設定。
+
+lesson4.php　5.　
+組み込み関数implode()の削除。
+回文判定時スペルが違う時点でループの終了するよう修正。
+if()の判定をfor()の中で判定しその時点で表示するよう修正。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lesson4.php
+++ lesson4.php
@@ -1,9 +1,9 @@
 <?php
 // 1. 1から100までの数値で、10の倍数だけを表示するプログラムを作成してください
 // 100回処理
-for($i=1; $i <= 100; $i++) {
+for ($i = 1; $i <= 100; $i++) {
     // 10の倍数を判定して表示
-    if($i % 10 === 0) {
+    if ($i % 10 === 0) {
         echo $i . "\n";
     }
 }
@@ -14,12 +14,11 @@ for($i=1; $i <= 100; $i++) {
 // 初期値(合計値)
 $total = 0;
 
-for($i = 1; $i <= 100; $i++) {
+for ($i = 1; $i <= 100; $i++) {
 
-    if($i % 2 === 0) { // 偶数のみ加算
+    if ($i % 2 === 0) { // 偶数のみ加算
         $total = $total + $i;
     }
-
 }
 // 合計値の表示
 echo $total;
@@ -31,20 +30,16 @@ echo $total;
 // 3の倍数 -> Fizz、5の倍数 -> Buzz、15の倍数 -> FizzBuzz、それ以外 -> 数字
 
 // 100以下の場合繰り返し処理
-for($i = 1; $i <= 100; $i++){
+for ($i = 1; $i <= 100; $i++) {
 
-    if($i % 15 === 0) { //　15の倍数を表示
+    if ($i % 15 === 0) { //　15の倍数を表示
         echo "FizzBuzz" . "\n";
-
-    } else if($i % 5 === 0) { //　5の倍数を表示
+    } else if ($i % 5 === 0) { //　5の倍数を表示
         echo "Buzz" . "\n";
-
-    } else if($i % 3 === 0) { //　3の倍数を表示
+    } else if ($i % 3 === 0) { //　3の倍数を表示
         echo "Fizz" . "\n";
-
     } else { //　それ以外の数字を表示
         echo $i . "\n";
-
     }
 }
 
@@ -61,9 +56,9 @@ $numbers = array(1, 2, 7, 4, 3);
 // 初期値　最も大きい数字
 $big_number = 0;
 
-foreach($numbers as $number) {// 入力された数字を取り出す
+foreach ($numbers as $number) { // 入力された数字を取り出す
 
-    if($number > $big_number) {//入力された数字が大きければ数字を代入する
+    if ($number > $big_number) { //入力された数字が大きければ数字を代入する
         $big_number = $number;
     }
 }
@@ -74,13 +69,14 @@ echo $big_number;
 
 
 
+
 <?php
 // // 5. 特定の文字列が回文かどうかを判別するプログラムを作成してください
 
 // // 回文の格納
-$word ="こんにんこ";
+$word = "んここん";
 
-// // 文字列を1文字づつ配列へ
+// // 回文を1文字づつ配列へ格納
 $array = mb_str_split($word);
 
 // 文字列数
@@ -89,20 +85,23 @@ $length = mb_strlen($word);
 // インデックスのカウント
 $index = $length;
 
-for($i = 0; $i < $length; $i++) {
+// 回文スペルチェック
+for ($i = 0; $i < $length; $i++) {
+
     // インデックスの最大数から-1
     $index = $index - 1;
-    // インデックスを逆順に格納
+    // 回文を逆順に格納
     $r_word[$i] = $array[$index];
-}
 
-// 回文の判定
-if($word === implode($r_word)){
-    echo "回文です";
-} else {
-    echo "回文ではありません";
+    //スペルが一致しない場合
+    if ($array[$i] !== $r_word[$i]) {
+        echo $word . "  は回文ではありません";
+        //1文字でも違えばスペルチェックの終了
+        break;
+    } else if ($i === $length - 1) {
+        // 配列回数分判定していて、判定ミスが無い場合
+        echo $word . "  は" . "回文です";
+    }
 }
-
 
 ?>
-
